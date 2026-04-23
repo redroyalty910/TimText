58b1b99981b6d79e213222e3134a1a1c19d4a502
Timothy Mills <Timothy.D.Mills@outlook.com>
2026-04-23 01:36:56 -0400

    implemented file save using FLTK file chooser and C++ file I/O

--- TimText/main.cpp
+++ TimText/main.cpp
@@ -4,10 +4,28 @@
 #include <FL/Fl_Text_editor.H> // for visible text editing widget
 #include <FL/Fl_Text_Buffer.H> // class for text buffer
 #include <FL/fl_ask.H> // a simple popup message box
+#include <Fl/fl_File_Chooser.H> // opens a file picker window in explorer
+#include <fstream> // file stream, lets C++ write to files
 
 void new_cb(Fl_Widget*, void* data) { // callback for file -> new
 	Fl_Text_Buffer* textbuf = (Fl_Text_Buffer*)data;
 	textbuf->text("");
+}
+
+void save_cb(Fl_Widget*, void* data) {
+	Fl_Text_Buffer* textbuf = (Fl_Text_Buffer*)data;
+	const char* filename = fl_file_chooser("Save File", "*.txt", ""); // let's user pick a file name in a new window
+	if (filename == nullptr) { // string if file path, nullptr if cancelled
+		return; // implies that the user cancelled
+	}
+	std::ofstream file(filename); // creates file output stream to write file
+	if (!file) {
+		fl_message("Hey bud, it's tim... we couldn't save your file.");
+		return;
+	}
+	file << textbuf->text(); // writes text from buffer to file
+	file.close();
+	fl_message("Your file has been 100% saved successfully!");
 }
 
 void copy_cb(Fl_Widget*, void* data) { // callback for edit -> copy
@@ -18,6 +36,16 @@ void copy_cb(Fl_Widget*, void* data) { // callback for edit -> copy
 void paste_cb(Fl_Widget*, void* data) { // callback for edit -> paste
 	Fl_Text_Editor* editor = (Fl_Text_Editor*)data;
 	Fl_Text_Editor::kf_paste(0, editor); // FLTK's built-in paste function
+}
+
+void cut_cb(Fl_Widget*, void* data) { // callback for edit -> cut
+	Fl_Text_Editor* editor = (Fl_Text_Editor*)data;
+	Fl_Text_Editor::kf_paste(0, editor); // FLTK's built-in cut function
+}
+
+void delete_cb(Fl_Widget*, void* data) { // callback for edit -> delete
+	Fl_Text_Editor* editor = (Fl_Text_Editor*)data;
+	Fl_Text_Editor::kf_delete(0, editor); // FLTK's built-in cut function
 }
 
 void about_cb(Fl_Widget*, void*) { // does exactly what you think
@@ -34,12 +62,14 @@ int main() {
 	Fl_Text_Buffer* textbuf = new Fl_Text_Buffer(); // where text lives; the document in memory	
 	Fl_Text_Editor* editor = new Fl_Text_Editor(0, 25, 800, 575); // typing area, displays/edits the buffer
 	editor->buffer(textbuf); // connects editor widet to text buffer
-		menu->add("&File/&New", 0, new_cb, textbuf); // parent menu / child item format, 0 = keyboard sc, whenclikced = runs callback above
+		menu->add("&File/&New", FL_CTRL + 'n', new_cb, textbuf); // parent menu / child item format, 0 = keyboard sc, whenclikced = runs callback above
 		menu->add("&File/&Open", 0, hello_cb);
-		menu->add("&File/&Save", 0, hello_cb);
+		menu->add("&File/&Save", FL_CTRL + 's', save_cb, textbuf);
 
+		menu->add("&Edit/&Cut", FL_CTRL + 'x', cut_cb, editor);
 		menu->add("&Edit/&Copy", 0, copy_cb, editor);
 		menu->add("&Edit/&Paste", 0, paste_cb, editor);
+		menu->add("&Edit/&Delete", 0, delete_cb, editor);
 
 		menu->add("&Help/&About", 0, about_cb);
 		menu->add("&Tim/&Tim", 0, hello_cb);

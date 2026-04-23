553cc0a066000608a2f8149e3db6a72d22187387
Timothy Mills <Timothy.D.Mills@outlook.com>
2026-04-23 19:06:20 -0400

    Added file open functionality, dynamic window title updates, and can now track unsaved changes

--- TimText/main.cpp
+++ TimText/main.cpp
@@ -6,24 +6,49 @@
 #include <FL/fl_ask.H> // a simple popup message box
 #include <Fl/fl_File_Chooser.H> // opens a file picker window in explorer
 #include <fstream> // file stream, lets C++ write to files
-#include <string>
+#include <string> // for the use of strings
+#include <sstream> // library for string streams
+
+struct EditorData { // core callbacks grouped together
+	Fl_Text_Buffer* textbuf; // because some callbacks need more than one object
+	Fl_Text_Editor* editor;
+	Fl_Window* window;
+};
 
 std::string current_file = ""; // empty string = no file yet / non-empty = already saved a file
 bool is_modified = false; // false? nothing changed. true? user edited text
 
-void modify_cb(int, int, int, int, const char*, void*) { // when text buffer changes this runs and marks doc as modified
-	is_modified = true;
+void update_title(Fl_Window* window) {
+	std::string title = "TimText --> "; // name of app "TimText"
+
+	if (current_file.empty()) { // check if file has a name
+		title += "Untitled"; // if not, labeled as "untitled"
+	}
+	else {
+		title += current_file; // if not empty, labels based on current file name
+	}
+	if (is_modified) {  // if unsaved changes exist
+		title += " *";
+	}
+	window->label(title.c_str()); // apply changes to the window
+}
+
+void modify_cb(int, int, int, int, const char*, void* data) { // when text buffer changes this runs and marks doc as modified
+	is_modified = true; // modify changes is_modified
+	Fl_Window* window = (Fl_Window*)data;
+	update_title(window); // modify updates the title
 }
 
 void new_cb(Fl_Widget*, void* data) { // callback for file -> new
-	Fl_Text_Buffer* textbuf = (Fl_Text_Buffer*)data;
-	textbuf->text(""); // clears text
+	EditorData* app = (EditorData*)data;
+	app->textbuf->text("");
 	current_file = ""; // forgets current file
 	is_modified = false; // resets to false
+	update_title(app->window); 
 }
 
 void open_cb(Fl_Widget*, void* data) { // callback for file -> open
-	Fl_Text_Buffer* textbuf = (Fl_Text_Buffer*)data;
+	EditorData* app = (EditorData*)data;
 
 	if (is_modified) {
 		int choice = fl_choice(
@@ -37,7 +62,7 @@ void open_cb(Fl_Widget*, void* data) { // callback for file -> open
 
 	const char* filename = fl_file_chooser("Open File", "*txt", ""); // opens file picker
 
-	if (filename == nullptr) { // if the user cancels
+	if (!filename) { // if the user cancels
 		return;
 	}
 
@@ -47,21 +72,19 @@ void open_cb(Fl_Widget*, void* data) { // callback for file -> open
 		fl_message("Dude I can't open that.");
 		return;
 	}
-	std::string contents;
-	std::string line;
+	std::stringstream buffer; // creates an empty stream called buffer
+	buffer << file.rdbuf(); // give raw content of file stream
 
-	while (std::getline(file, line)) { // get line, add to contents, add a newline after it
-		contents += line + '\n';
-	}
-	file.close();
-	textbuf->text(contents.c_str()); // puts text into the editor buffer
-	current_file = filename; // remember the file path
-	fl_message("I opened your file bro.");
-	is_modified = false; // reset mod value to false
+	app->textbuf->text(buffer.str().c_str());
+
+	current_file = filename;
+	is_modified = false;
+	update_title(app->window);
+	fl_message("Your file has been opened.");
 }
 
 void save_cb(Fl_Widget*, void* data) { // callback for file -> save
-	Fl_Text_Buffer* textbuf = (Fl_Text_Buffer*)data;
+	EditorData* app = (EditorData*)data;
 	if(current_file.empty()) { // if there is no file, ask for one
 		const char* filename = fl_file_chooser("Save File", "*.txt", ""); // let's user pick a file name in a new window
 
@@ -74,17 +97,18 @@ void save_cb(Fl_Widget*, void* data) { // callback for file -> save
 		fl_message("Hey bud, it's tim... we couldn't save your file.");
 		return;
 	}
-	file << textbuf->text(); // writes text from buffer to file
+	file << app->textbuf->text(); // writes text from buffer to file
 	file.close();
-	fl_message("Your file has been 100% saved successfully!");
 	is_modified = false;
+	update_title(app->window);
+	fl_message("Your file has been 100% saved successfully!");
 }
 
 void save_as_cb(Fl_Widget*, void* data) { // callback for file -> save as
-	Fl_Text_Buffer* textbuf = (Fl_Text_Buffer*)data;
+	EditorData* app = (EditorData*)data;
 		const char* filename = fl_file_chooser("Save File As", "*.txt", ""); // let's user pick a file name in a new window
 
-		if (filename == nullptr) // string if file path, nullptr if cancelled
+		if (!filename) // string if file path, nullptr if cancelled
 			return; // implies that the user cancelled
 		current_file = filename;
 
@@ -94,30 +118,31 @@ void save_as_cb(Fl_Widget*, void* data) { // callback for file -> save as
 		fl_message("Hey bud, it's tim... we couldn't save your file.");
 		return;
 	}
-	file << textbuf->text(); // writes text from buffer to file
+	file << app->textbuf->text(); // writes text from buffer to file
 	file.close();
-	fl_message("Your file has been 100% saved successfully!");
 	is_modified = false; // reset modification to false
+	fl_message("Your file has been 100% saved successfully!");
+
 }
 
 void copy_cb(Fl_Widget*, void* data) { // callback for edit -> copy
-	Fl_Text_Editor* editor = (Fl_Text_Editor*)data;
-	Fl_Text_Editor::kf_copy(0, editor); // FLTK's built-in copy function
+	EditorData* app = (EditorData*)data;
+	Fl_Text_Editor::kf_copy(0, app->editor); // FLTK's built-in copy function
 }
 
 void paste_cb(Fl_Widget*, void* data) { // callback for edit -> paste
-	Fl_Text_Editor* editor = (Fl_Text_Editor*)data;
-	Fl_Text_Editor::kf_paste(0, editor); // FLTK's built-in paste function
+	EditorData* app = (EditorData*)data;
+	Fl_Text_Editor::kf_paste(0, app->editor); // FLTK's built-in paste function
 }
 
 void cut_cb(Fl_Widget*, void* data) { // callback for edit -> cut
-	Fl_Text_Editor* editor = (Fl_Text_Editor*)data;
-	Fl_Text_Editor::kf_paste(0, editor); // FLTK's built-in cut function
+	EditorData* app = (EditorData*)data;
+	Fl_Text_Editor::kf_paste(0, app->editor); // FLTK's built-in cut function
 }
 
 void delete_cb(Fl_Widget*, void* data) { // callback for edit -> delete
-	Fl_Text_Editor* editor = (Fl_Text_Editor*)data;
-	Fl_Text_Editor::kf_delete(0, editor); // FLTK's built-in cut function
+	EditorData* app = (EditorData*)data;
+	Fl_Text_Editor::kf_delete(0, app->editor); // FLTK's built-in cut function
 }
 
 void about_cb(Fl_Widget*, void*) { // does exactly what you think
@@ -132,24 +157,26 @@ int main() {
 	Fl_Window* window = new Fl_Window(800, 600, "TimText"); // main app window
 	Fl_Menu_Bar* menu = new Fl_Menu_Bar(0, 0, 800, 25); // bar at top of GUI
 	Fl_Text_Buffer* textbuf = new Fl_Text_Buffer(); // where text lives; the document in memory	
-	textbuf->add_modify_callback(modify_cb, nullptr);
+	textbuf->add_modify_callback(modify_cb, window);
 	Fl_Text_Editor* editor = new Fl_Text_Editor(0, 25, 800, 575); // typing area, displays/edits the buffer
 	editor->buffer(textbuf); // connects editor widet to text buffer
-		menu->add("&File/&New", FL_CTRL + 'n', new_cb, textbuf); // parent menu / child item format, 0 = keyboard sc, whenclikced = runs callback above
-		menu->add("&File/&Open", FL_CTRL + 'o', open_cb, textbuf);
-		menu->add("&File/&Save", FL_CTRL + 's', save_cb, textbuf);
-		menu->add("&File/Save &As", FL_CTRL + 'a', save_as_cb, textbuf);
+	EditorData* app = new EditorData{ textbuf, editor, window };
+		menu->add("&File/&New", FL_CTRL + 'n', new_cb, app); // parent menu / child item format, 0 = keyboard sc, whenclikced = runs callback above
+		menu->add("&File/&Open", FL_CTRL + 'o', open_cb, app);
+		menu->add("&File/&Save", FL_CTRL + 's', save_cb, app);
+		menu->add("&File/Save &As", FL_CTRL + 'a', save_as_cb, app);
 
-		menu->add("&Edit/&Cut", FL_CTRL + 'x', cut_cb, editor);
-		menu->add("&Edit/&Copy", 0, copy_cb, editor);
-		menu->add("&Edit/&Paste", 0, paste_cb, editor);
-		menu->add("&Edit/&Delete", 0, delete_cb, editor);
+		menu->add("&Edit/&Cut", FL_CTRL + 'x', cut_cb, app);
+		menu->add("&Edit/&Copy", 0, copy_cb, app);
+		menu->add("&Edit/&Paste", 0, paste_cb, app);
+		menu->add("&Edit/&Delete", 0, delete_cb, app);
 
 		menu->add("&Help/&About", 0, about_cb);
 		menu->add("&Tim/&Tim", 0, hello_cb);
 		menu->add("&Tim/&Tim", 0, hello_cb);
 
 	window->end(); // nothing else to add to the window
+	update_title(window); // when file state changes, update title
 	window->show(); // tells FLTK to make the window visible
 
 	return Fl::run(); // keeps the program alive
